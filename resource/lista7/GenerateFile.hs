b710f4d94f25cc6c43e75959d7d5e314ea3c081c
unknown <rafael.sousa@ccc.ufcg.edu.br>
2018-02-24 02:28:13 -0300

    Init automatic corretion issue

---
+++ resource/lista7/GenerateFile.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, DeriveAnyClass #-}
+
+module GenerateFile (main) where
+
+import TestIntersection as In
+import TestUnion as Un
+import TestRemove as Rm
+import TestInsert as Insert
+import TestInclusion as Inclusion
+import TestMinus as Minus
+import TestSize as Size
+import TestSearch as Search
+import TestSum as Sum
+import Test.HUnit
+import Data.List.Split
+import GHC.Generics
+import System.IO as T
+import Data.Text.Lazy (Text)
+import Data.Text.Lazy.IO as I
+import Data.Aeson.Text (encodeToLazyText)
+import Data.Aeson (ToJSON)
+
+data Output = Output { matricula :: String, falhas :: Int,
+                    passaram :: Int, totalTestes :: Int, excecoes :: Int} deriving (Show, Generic, ToJSON)
+
+
+main :: String -> IO ()
+main matricula = do
+  testsResult <- runTestTT $ test $ mconcat [ Insert.tests, Inclusion.tests, Rm.tests, Minus.tests, Size.tests, Search.tests, Sum.tests, In.tests, Un.tests]
+  generateResult matricula testsResult
+
+generateResult :: String -> Counts -> IO ()
+generateResult matricula testsResult = do
+  T.putStrLn "Write file in result directory json worked"
+  let fileName = "results/" ++ matricula ++ ".json"
+  let totalTestes = cases testsResult
+  let passaram = tried testsResult - errors testsResult - failures testsResult
+  let excecoes = errors testsResult
+  let falhas = failures testsResult
+  let output = Output { matricula = matricula, falhas = falhas, passaram = passaram,
+                          totalTestes = totalTestes,  excecoes = excecoes}
+  I.writeFile (fileName) (encodeToLazyText output)
+  T.putStrLn "Write file in result directory json worked"
--- src/Handler/Submission.hs
+++ src/Handler/Submission.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric, ScopedTypeVariables, DeriveAnyClass #-}
 module Handler.Submission where
 
+import System.Process
 import Import
+import System.Directory
+import System.IO as T
+import Control.Exception
 
 data ResponseMessage = ResponseMessage { message :: String} deriving (Show, Generic)
 
@@ -18,15 +22,38 @@ rm_worked  = ResponseMessage { message = "File was successfully uploaded"}
 rm_error :: ResponseMessage
 rm_error = ResponseMessage { message = "Something wrong happened"}
 
-postSubmissionR :: Handler Value
+try' :: IO a ->  IO (Either IOException a)
+try' =  Control.Exception.try
+
+postSubmissionR :: Import.Handler Value
 postSubmissionR = do
   listNumber <- runInputPost $ ireq textField "listNumber"
   result <- runInputPost $ iopt fileField "studentSubmission"
 
   case result of Just fileInfo -> do
-                    let mounthInitalPath = "resource/" <> listNumber <> "/"
+                    let mounthInitalPath = "resource/" <> listNumber <> "/students/"
                     let destination = (unpack $ mounthInitalPath <> (fileName fileInfo) )
                     liftIO $ fileMove fileInfo destination
+                    initCorrection destination listNumber
+                    initCommand "fsdfs"
                     sendResponseStatus status200 $ toJSON rm_worked
                  Nothing -> do
                     sendResponseStatus status400 $ toJSON rm_error
+
+
+initCorrection :: FilePath -> Text -> HandlerT App IO ()
+initCorrection filePath listNumber = do
+  fileContent <- liftIO $ T.readFile filePath
+  writeToFile fileContent listNumber
+
+writeToFile :: String -> Text -> HandlerT App IO ()
+writeToFile fileContent listNumber = do
+  liftIO $ T.writeFile "resource/lista7/MultisetMap.hs" fileContent
+
+initCommand :: String -> HandlerT App IO ()
+initCommand registration = do
+  let cmd = "ghci ./resource/lista7/GenerateFile.hs -e \"main \\\"" Prelude.++ registration Prelude.++ "\\\"\""
+  result <- liftIO $ try' (callCommand cmd)
+  case result of
+    Left ex -> liftIO $ T.putStrLn "started ok"
+    Right () -> liftIO $ T.putStrLn "started ok"
