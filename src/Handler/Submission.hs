1b2ba75945a84a0d08a88f6d56b664ae96362470
unknown <rafael.sousa@ccc.ufcg.edu.br>
2018-03-05 12:37:05 -0300

    Now the app can handle insert submission and get Submission as json

--- src/Handler/Submission.hs
+++ src/Handler/Submission.hs
@@ -36,7 +36,7 @@ rmError = ResponseMessage { message = "Something wrong happened" }
 try' :: IO a ->  IO (Either IOException a)
 try' =  Control.Exception.try
 
-postSubmissionR :: Import.Handler Value
+postSubmissionR :: Import.Handler ()
 postSubmissionR = do
   listNumber <- runInputPost $ ireq textField "listNumber"
   result <- runInputPost $ iopt fileField "studentSubmission"
@@ -47,7 +47,7 @@ postSubmissionR = do
                     let destination = unpack $ mounthInitalPath <> fileName fileInfo
                     liftIO $ fileMove fileInfo destination
                     initCorrection destination listNumber
-                    liftIO $ initCommand (unpack studentId)
+                    initCommand (unpack studentId)
                     sendResponseStatus status200 $ toJSON rmWorked
                  Nothing -> sendResponseStatus status400 $ toJSON rmError
 
@@ -61,26 +61,26 @@ writeToFile :: String -> String -> HandlerT App IO ()
 writeToFile fileContent listNumber =
   liftIO $ T.writeFile ("resource/" Prelude.++ listNumber Prelude.++ "/MultisetMap.hs") fileContent
 
+initCommand :: String -> HandlerT App IO ()
 initCommand registration = do
   let cmd = "ghci ./resource/lista7/GenerateFile.hs -iresource/lista7 -e \"main \\\"" Prelude.++ registration Prelude.++ "\\\"\""
   result <- liftIO $ try' (callCommand cmd)
   case result of
       Left ex -> liftIO $ T.print ex
       Right () -> do
-                    contents <- Prelude.readFile $ "./resource/lista7/results/" Prelude.++ registration Prelude.++ ".json"
+                    contents <- liftIO $ Prelude.readFile $ "./resource/lista7/results/" Prelude.++ registration Prelude.++ ".json"
                     let readJson = decode $ C.pack contents :: Maybe Submission
                     case readJson of
                         Just (Submission studentId failed passed total exceptions listName) -> do
-                            let subId = runDB $ insert (Submission studentId failed passed total exceptions listName) :: HandlerT App IO (Key Submission)
+                            runDB $ insert $ Submission studentId failed passed total exceptions listName
                             -- let sub2 = runDB $ getBy $ SubKey studentId listName :: HandlerT App IO (Maybe (Entity Submission))
-                            subId
                             liftIO $ T.print exceptions
                             liftIO $ T.print failed
                             liftIO $ T.print passed
                             liftIO $ T.print total
                             liftIO $ T.print exceptions
 
-                        _ -> liftIO $ T.print "Nao pegou"
+                        _ -> liftIO $ T.print "Not Worked"
 
 getSubmissionR :: Import.Handler Value
 getSubmissionR = do
